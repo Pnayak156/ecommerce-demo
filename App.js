59b026ae380f4844f1afe295675b3acd707dff3e
Prashanta Nayak <prashanta.nayak@myntra.com>
2019-07-16 16:58:13 +0530

    Add searchList

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Home from './src/apps/Home';
+// import Home from './src/apps/Home';
+import Search from './src/apps/Search';
 
 export default class App extends React.Component {
 	
 	render() {
 		return (
-			<Home/>
+			<Search/>
 		);
 	}
 }
